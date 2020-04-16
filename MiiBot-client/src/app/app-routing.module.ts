fefa49364345cc7c43a24e1267683ef408508115
alberti-tu <albertalvarezmiron@gmail.com>
2020-04-16 20:50:27 +0200

    AuthGuard

--- MiiBot-client/src/app/app-routing.module.ts
+++ MiiBot-client/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationService } from './services/authentication.service';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthenticationService] },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
 
--- MiiBot-client/src/app/services/authentication.service.ts
+++ MiiBot-client/src/app/services/authentication.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
 import * as crypto from 'crypto-js';
-import { Router } from '@angular/router';
 
 const key = 'token'; 
 
 @Injectable({ providedIn: 'root' })
-export class AuthenticationService {
+export class AuthenticationService implements CanActivate {
 
   constructor(private router: Router) { }
 
-  public getToken(): string {
-    return localStorage.getItem(key);
+  public canActivate(): boolean {
+    if (this.getToken() !== null) {
+      return true;
+    } else {
+      this.removeToken();
+      return false;
+    }
   }
 
   public saveToken(value: string): void {
     localStorage.setItem(key, value);
     this.router.navigateByUrl('/home');
+  }
+
+  public getToken(): string {
+    return localStorage.getItem(key);
   }
 
   public removeToken(): void {
