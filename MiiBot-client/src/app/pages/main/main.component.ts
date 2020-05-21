35f4c48cfafb4ea8c23d0e7a1fc1552d3f87a6c5
alberti-tu <albertalvarezmiron@gmail.com>
2020-05-21 17:28:01 +0200

    Menu List

--- MiiBot-client/src/app/pages/main/main.component.ts
+++ MiiBot-client/src/app/pages/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatSidenav } from '@angular/material/sidenav';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-main',
@@ -11,20 +12,25 @@ export class MainComponent implements OnInit {
 
   @ViewChild('sideMenu') sideMenu: MatSidenav;
 
-  public pageList: { name: string, url: string }[] = [];
+  public pageList: { name: string, icon: string, action: () => void }[] = [];
 
-  constructor(private router: Router) {}
+  constructor(private authService: AuthenticationService, private router: Router) {}
 
   public ngOnInit(): void {
     this.pageList = [
-      { name: 'Home', url: '/home' },
-      { name: 'Register', url: '/register' }
+      { name: 'Home', icon: 'home', action: () => this.navigateTo('/home') },
+      { name: 'Register', icon: 'person_add', action: () => this.navigateTo('/register') },
+      { name: 'Cerrar sesión', icon: 'directions_run', action: () => this.authService.removeToken() }
     ];
   }
 
-  public navigateTo(page: { name: string, url: string }) {
+  public navigateTo(url: string) {
     this.sideMenu.close();
-    this.router.navigateByUrl(page.url);
+    this.router.navigateByUrl(url);
+  }
+
+  public closeSession() {
+    this.authService.removeToken();
   }
 
 }
